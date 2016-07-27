c0cad0da6fd4f6bf26f5b87b154bb2a5c67d46bc
Silvio <silvio.fiorito@granturing.com>
2016-07-27 10:47:58 -0400

    adding a dataframe

--- src/Source Control Test.scala
+++ src/Source Control Test.scala
@@ -1,6 +1,14 @@
-// Databricks notebook source exported at Thu, 7 Jul 2016 18:58:17 UTC
+// Databricks notebook source exported at Wed, 27 Jul 2016 14:47:55 UTC
 val i = sc.parallelize(1 to 100)
 
 // COMMAND ----------
 
 i.reduce(_ + _)
+
+// COMMAND ----------
+
+val df = sqlContext.createDataFrame(Seq((1, "Joe"), (2, "Bob"))).toDF("id", "name")
+display(df)
+
+// COMMAND ----------
+
